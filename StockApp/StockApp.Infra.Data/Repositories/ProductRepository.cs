e856d28431636564d07c063583e1e989c2b0b8c9
ALAN ALTO <alan.alto@fatec.sp.gov.br>
2024-04-08 12:30:26 -0300

    productRepository

---
+++ StockApp/StockApp.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockApp.Domain.Entities;
+using StockApp.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Infra.Data.Repositories
+{
+    public class ProductRepository
+    {
+        private ApplicationDbContext _productContext;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContext = context;
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            _productContext.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetById(int? id)
+        {
+            var product = await _productContext.Products.FindAsync(id);
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _productContext.Products.OrderBy(c => c.Name).ToListAsync();
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            _productContext.Update(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> Remove(Product product)
+        {
+            _productContext.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+    }
+}

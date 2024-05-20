92cb2e39ba4ad95bc28dfcafbb32fa3b70b99058
victoricoma-soulcode <victor.icoma@soulcode.com>
2024-05-20 12:36:19 -0300

    Aula2005 Application para Migration

---
+++ StockApp/StockApp.Application/Mappings/DTOToCommandMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using StockApp.Application.DTOs;
+using StockApp.Application.Products.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.Mappings
+{
+    public class DTOToCommandMappingProfile : Profile
+    {
+        public DTOToCommandMappingProfile()
+        {
+            CreateMap<ProductDTO, ProductCreateCommand>();
+            CreateMap<ProductDTO, ProductUpdateCommand>();
+        }
+    }
+}
